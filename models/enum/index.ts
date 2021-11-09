61beae7de9ced79fd4fc63fda667db341f8f1c07
Lodewijk Hertog <loekhertog@gmail.com>
2021-11-09 12:11:22 +0100

    add coreServicesMachineClient

--- models/enum/index.ts
+++ models/enum/index.ts
@@ -1,3 +1,4 @@
 export { default as MimeType } from './MimeType'
 export { default as OAuthErrorCode } from './OAuthErrorCode'
 export { default as HttpStatus } from './HttpStatus'
+export { default as CoreServiceStack } from './CoreServiceStack'
--- services/client/index.ts
+++ services/client/index.ts
@@ -1,5 +1,6 @@
 export { default as MachineClient } from './MachineClient'
 export { default as MachineTokenClient } from './MachineTokenClient'
+export { default as CoreServicesMachineClient } from './CoreServicesMachineClient'
 export { default as OAuthClient } from './OAuthClient'
 export { default as CognitoOAuthClient } from './CognitoOAuthClient'
 export { default as AxiosClient } from './AxiosClient'
