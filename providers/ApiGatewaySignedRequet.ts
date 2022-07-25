b7cb2df3928e49e2a0c3a926aaf74c07db216a02
Loek Hertog <loekhertog@gmail.com>
2022-07-25 16:15:34 +0200

    use different package for axios signing

--- providers/ApiGatewaySignedRequet.ts
+++ providers/ApiGatewaySignedRequet.ts
@@ -1,7 +1,4 @@
-import { SignatureV4 } from '@aws-sdk/signature-v4'
-import { fromTemporaryCredentials } from '@aws-sdk/credential-providers'
-import { randomUUID } from 'crypto'
-import { Sha256 } from '@aws-crypto/sha256-js'
+import { aws4Interceptor, InterceptorOptions } from 'aws4-axios'
 
 import ServiceProvider, {
   Lifecycle,
@@ -11,10 +8,8 @@ import { HttpClient } from '../services/client'
 import AwsV4SignedRequestClient from '../services/client/AwsV4SignedRequestClient'
 
 const configureApiGatewaySignedRequestProvider = (config: {
-  baseUrl: string
-  stsRoleArn: string
   httpClientSymbol: ServiceSymbol<HttpClient>
-  region: string
+  interceptorOptions: InterceptorOptions
 }): ServiceProvider => {
   const provider = new ServiceProvider()
 
@@ -22,20 +17,7 @@ const configureApiGatewaySignedRequestProvider = (config: {
     config.httpClientSymbol,
     Lifecycle.Singleton,
     () =>
-      new AwsV4SignedRequestClient(
-        config.baseUrl,
-        new SignatureV4({
-          credentials: fromTemporaryCredentials({
-            params: {
-              RoleArn: config.stsRoleArn,
-              RoleSessionName: randomUUID(),
-            },
-          }),
-          service: 'execute-api',
-          region: config.region,
-          sha256: Sha256,
-        }),
-      ),
+      new AwsV4SignedRequestClient(aws4Interceptor(config.interceptorOptions)),
   )
 
   return provider
--- services/client/AwsV4SignedRequestClient.ts
+++ services/client/AwsV4SignedRequestClient.ts
@@ -1,52 +1,21 @@
-import { SignatureV4 } from '@aws-sdk/signature-v4'
+import { aws4Interceptor } from 'aws4-axios'
 import { AxiosRequestConfig } from 'axios'
-import { HttpRequest } from '@aws-sdk/protocol-http'
-import { URL } from 'url'
 
 import AxiosClient from './AxiosClient'
 
 class AwsV4SignedRequestClient extends AxiosClient {
-  private async signRequestInterceptor(
-    req: AxiosRequestConfig,
-  ): Promise<AxiosRequestConfig> {
-    let body: string | undefined
-    const hasBody = !!req.data
-    if (hasBody) {
-      body = JSON.stringify(req.data)
-    }
-
-    const method = req.method?.toUpperCase() as string
-    const url = new URL(req.url as string)
-
-    const httpRequest = new HttpRequest({
-      method,
-      hostname: url.hostname,
-      path: url.pathname,
-      headers: req.headers,
-      body,
-      query: req.params,
-    })
-
-    const signedRequest = await this.signer.sign(httpRequest, {
-      signingDate: new Date(),
-    })
-
-    req.data = signedRequest.body
-    req.headers = signedRequest.headers
-    req.params = signedRequest.query
-
-    return req
+  private signRequestInterceptor = (
+    requestConfig: AxiosRequestConfig,
+  ): Promise<AxiosRequestConfig> => {
+    return this.interceptor(requestConfig)
   }
 
   public constructor(
-    private readonly baseUrl: string,
-    private readonly signer: SignatureV4,
+    private readonly interceptor: ReturnType<typeof aws4Interceptor>,
   ) {
     super()
 
-    this.axiosClient.interceptors.request.use((req) =>
-      this.signRequestInterceptor(req),
-    )
+    this.axiosClient.interceptors.request.use(this.signRequestInterceptor)
   }
 }
 
